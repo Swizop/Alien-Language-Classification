d0a768a296821bca457050cefbda3bbe2d30de1a
Matei Neagu <t-mateineagu@microsoft.com>
2022-05-14 22:06:49 +0300

    first submission

---
+++ main.py
@@ -0,0 +1,60 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import preprocessing, metrics
+from sklearn.naive_bayes import MultinomialNB
+
+# trainLabels = np.loadtxt('alien_language/train_samples.txt', delimiter='\t', dtype='str', encoding='utf-8')
+# print(trainLabels[1][30])
+# trainSamples = np.loadtxt('alien_language/train_labels.txt')
+f = open("alien_language/train_samples.txt", encoding="utf-8")
+f = f.readlines()
+g = open("alien_language/train_labels.txt", encoding="utf-8")
+g = g.readlines()
+countVectorizer = CountVectorizer(max_features=20000)
+
+def preprocess_and_normalize(f, g, trainCall=True):
+    global countVectorizer
+    trainSamplesIds = []
+    trainSamples = []
+    trainLabelsIds = []
+    trainLabels = []
+    i = 0
+    for line in f:
+        lineSplit = line.split("\t")
+        trainSamplesIds.append(lineSplit[0])
+        trainSamples.append(lineSplit[1][:-1])
+        
+        lineSplit = g[i].split("\t")
+        trainLabelsIds.append(lineSplit[0])
+        trainLabels.append(int(lineSplit[1][:-1]))
+        i += 1
+
+    trainLabels = np.array(trainLabels)
+    trainSamples = np.array(trainSamples)
+    
+    if trainCall == True:
+        # print(trainSamples.shape)
+        countVectorizer.fit(trainSamples)
+    trainSamples = countVectorizer.transform(trainSamples).toarray()
+    # trainSamples = preprocessing.normalize(trainSamples, norm='l2')
+    
+    return trainSamples, trainLabels
+
+# print(type(trainSamples))
+# print(trainSamples[0].sum())
+# print(trainSamples[0].sum())
+
+trainSamples, trainLabels = preprocess_and_normalize(f, g)
+f = open("alien_language/validation_samples.txt", encoding="utf-8")
+f = f.readlines()
+g = open("alien_language/validation_labels.txt", encoding="utf-8")
+g = g.readlines()
+valSamples, valLabels = preprocess_and_normalize(f, g, False)
+
+
+nbModel = MultinomialNB()
+nbModel.fit(trainSamples, trainLabels)
+nbModel.predict(valSamples)
+print(nbModel.score(valSamples, valLabels))
+# predictions = nbModel.predict(valSamples)
+# print(metrics.accuracy_score(valLabels, predictions))
